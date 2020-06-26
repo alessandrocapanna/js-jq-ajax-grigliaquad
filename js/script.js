050525ce40dc26c1eea766eaf8c1dc528fd36cb4
alessandrocapanna <acapanna75@gmail.com>
2020-06-26 10:26:42 +0200

    con verifica 'se il quadrato è già stato cliccato'

--- js/script.js
+++ js/script.js
@@ -18,44 +18,51 @@ $(document).ready(
       function() {
         var quadratoCliccato = $(this);
 
-        // ajax
-        $.ajax(
-          {
-            // endpoint API  method
-            url: 'https://flynn.boolean.careers/exercises/api/random/int',
-            method: 'GET',
+        // controllo se è gia stato cliccato se si faccio alert se no faccio chiamata ajax etc
+        if (quadratoCliccato.hasClass('cliccato')) {
+          alert('gia stato cliccato mi disp')
+        }else {
+          // ajax
+          $.ajax(
+            {
+              // endpoint API  method
+              url: 'https://flynn.boolean.careers/exercises/api/random/int',
+              method: 'GET',
 
-            // in caso di successo
-            success: function(numeroRandom) {
-              // Inserisco numero random nella variabile
-              var numero = numeroRandom.response;
+              // in caso di successo
+              success: function(numeroRandom) {
+                // Inserisco numero random nella variabile
+                var numero = numeroRandom.response;
 
-              quadratoCliccato.find('.number').text(numero)
+                quadratoCliccato.find('.number').text(numero)
 
-              // Se il numero è < o uguale a 5 il quadrato diventa giallo
-              if (numero <= 5) {
-                // resetto  classi
-                quadratoCliccato.removeClass('yellow');
-                quadratoCliccato.removeClass('green');
+                // Se il numero è < o uguale a 5 il quadrato diventa giallo
+                if (numero <= 5) {
+                  // resetto  classi
+                  quadratoCliccato.removeClass('yellow');
+                  quadratoCliccato.removeClass('green');
 
-                // aggiungo classe
-                quadratoCliccato.addClass('yellow');
-              } else { //altrimenti diventa verde
-                // resetto  classi
-                quadratoCliccato.removeClass('yellow');
-                quadratoCliccato.removeClass('green');
+                  // aggiungo classe
+                  quadratoCliccato.addClass('yellow');
+                  quadratoCliccato.addClass('cliccato');
+                } else { //altrimenti diventa verde
+                  // resetto  classi
+                  quadratoCliccato.removeClass('yellow');
+                  quadratoCliccato.removeClass('green');
 
-                // Aggiungo classe
-                quadratoCliccato.addClass('green');
+                  // Aggiungo classe
+                  quadratoCliccato.addClass('green');
+                  quadratoCliccato.addClass('cliccato');
+                }
+              },
+              error:
+              function() {
+                alert('Errore');
               }
-            },
-            error:
-            function() {
-              alert('Errore');
             }
-          }
 
-        );
+          );
+        }
       }
     );
   }
